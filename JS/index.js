6b31193d60b11bc89a1cb589e80409baa0958fc4
4400191-afk <4400191@alu.murciaeduca.es>
2026-05-19 17:50:19 +0200

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -3,7 +3,7 @@ document.getElementById("btnDescarga").addEventListener("click", function () {
     const enlace = document.createElement("a");
 
     enlace.href = "../main.py";
-    enlace.download = "/main.py";
+    enlace.download = "../main.py";
 
     document.body.appendChild(enlace);
     enlace.click();
